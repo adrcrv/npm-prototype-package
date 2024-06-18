66366e503093b80de1661313de785dbd41d065e7
Andre Carvalho <adrcrv@gmail.com>
2024-06-17 23:02:01 -0300

    feat: add cli script

--- src/scripts/cli.sh
+++ src/scripts/cli.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+BASEDIR=$(dirname $0)
 COMMAND="$1"
 
 if [ "$COMMAND" == "postinstall" ]; then
-    ./scripts/postinstall.sh
+    chmod +x "${BASEDIR}/postinstall.sh"
+    "${BASEDIR}/../scripts/postinstall.sh"
 else
     echo "Invalid command. Available options: postinstall"
     exit 1
